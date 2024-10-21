7185d6bdf7b6c77fcdefb0eedb1d9a89cec4090b
Jagger85 <andresfonsortega@yahoo.es>
2024-10-21 12:53:49 +0700

    Feat(Model): Model class for job listings table entries

---
+++ models/JobListing.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class JobListing:
+    site: str
+    job_title: str
+    company_name: str
+    job_location: str
+    job_base_pay_range: str
+    job_posted_date: str
+    job_employment_type: str
+    url: str
