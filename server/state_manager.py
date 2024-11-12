ab4d9f0e8d5913c9a28b9ca0d7653525788b6ae0
Jagger85 <andresfonsortega@yahoo.es>
2024-11-12 08:14:59 +0700

    Feat(state_manager): Added a reset method to get back to initial status

--- server/state_manager.py
+++ server/state_manager.py
@@ -10,9 +10,14 @@ class StateManager:
             cls._instance = super(StateManager, cls).__new__(cls)
             cls._instance.platform_states = {}
             cls._instance.sse_observer = SSEObserver(get_sse_handler())
+            cls._instance.initialize_platform_states()
         return cls._instance
 
-    
+    def initialize_platform_states(self):
+        for platform in Platforms:
+            self.platform_states[platform] = PlatformStates.IDLE
+            self.sse_observer.notify_platform_state(platform,PlatformStates.IDLE)
+            
     def set_platform_state(self, platform: Platforms, state: PlatformStates):
         self.platform_states[platform] = state
         self.sse_observer.notify_platform_state(platform, state)
