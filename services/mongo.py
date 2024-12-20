7c78678d2957f25ffef35f0027cc07173cabcfcf
Jagger85 <andresfonsortega@yahoo.es>
2024-12-20 14:46:01 +0700

    Feat(mongo): The singleton now returns the database

--- services/mongo.py
+++ services/mongo.py
@@ -3,12 +3,12 @@ from constants import environment
 
 class MongoClient:
     _instance = None
+    _db = None
 
-    def __new__(cls):
-        if cls._instance is None:
-            connection_string = f"mongodb+srv://{environment.MONGO_USER}:{environment.MONGO_PASSWORD}@jobsweebcluster0.x1z6x.mongodb.net/?retryWrites=true&w=majority&appName=JobsweebCluster0"
-            cls._instance = PyMongoClient(connection_string)
-        return cls._instance
-
-# Usage
-mongo_client = MongoClient()
+    @classmethod
+    def get_db(cls):
+        if cls._db is None:
+            connection_string = f"mongodb+srv://{environment['mongo_user']}:{environment['mongo_password']}@jobsweebcluster0.x1z6x.mongodb.net/Jobsweep-Database?retryWrites=true&w=majority&appName=JobsweebCluster0"
+            client = PyMongoClient(connection_string)
+            cls._db = client['Jobsweep-Database']
+        return cls._db
