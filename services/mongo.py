cfdcb7b3dc5e71f37be28b35673e586c9f9c0906
Jagger85 <andresfonsortega@yahoo.es>
2024-12-20 21:18:56 +0700

    Feat(Mongo): Added the UserModel to the MongoClient

--- services/mongo.py
+++ services/mongo.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient as PyMongoClient
 from constants import environment
+from models import UserModel
 
 class MongoClient:
     _instance = None
     _db = None
+    user_model = None 
+    
 
     @classmethod
     def get_db(cls):
@@ -11,4 +14,5 @@ class MongoClient:
             connection_string = f"mongodb+srv://{environment['mongo_user']}:{environment['mongo_password']}@jobsweebcluster0.x1z6x.mongodb.net/Jobsweep-Database?retryWrites=true&w=majority&appName=JobsweebCluster0"
             client = PyMongoClient(connection_string)
             cls._db = client['Jobsweep-Database']
+            cls.user_model = UserModel(cls._db)
         return cls._db
