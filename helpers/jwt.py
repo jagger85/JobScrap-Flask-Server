a23de1392f322aff3d504c9ce3ea67afedfc52d7
Jagger85 <andresfonsortega@yahoo.es>
2024-12-22 17:04:59 +0700

    Fix(jwt): return modified and module initialization

--- helpers/jwt.py
+++ helpers/jwt.py
@@ -3,8 +3,8 @@ from constants import environment
 
 def get_user(token):
     payload = jwt.decode(token.split(" ")[1], environment["jwt_secret"], algorithms=["HS256"])
-    return payload.get('user')
+    return payload['username']
 
 def get_role(token):
     payload = jwt.decode(token.split(" ")[1], environment["jwt_secret"], algorithms=["HS256"])
-    return payload.get('role')
+    return payload['role']
