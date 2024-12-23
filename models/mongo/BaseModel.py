d8fc745b4942259310d1d25b78c4dfbd6e102a87
Jagger85 <andresfonsortega@yahoo.es>
2024-12-23 16:56:02 +0700

    Fix(BaseModel): Added the create function and fixed an indentation error in the validate_data function

--- models/mongo/BaseModel.py
+++ models/mongo/BaseModel.py
@@ -1,10 +1,14 @@
 class BaseModel:
     def __init__(self, db, collection_name):
         self.collection = db[collection_name]
+    
+    def create(self, data):
+        result = self.collection.insert_one(data)
+        return str(result.inserted_id)
 
     def validate_data(self, data, required_fields):
         for field in required_fields:
             if field not in data:
                 print(f"value missing {field}")
-            return False
+                return False
         return True
